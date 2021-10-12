6101803516f45c4b3788e03dfcfe00efaea79803
MaxR522 <ranjamario@gmail.com>
2021-10-12 20:31:26 +0300

    📖 DOC: fix documentation

--- public/doc/api_project.js
+++ public/doc/api_project.js
@@ -3,7 +3,7 @@ define({
   "version": "1.0.0",
   "description": "It's a small API made with node typescript",
   "url": "http://localhost:7777",
-  "sampleUrl": "http://localhost:7777",
+  "sampleUrl": null,
   "template": {
     "forceLanguage": "en"
   },
@@ -11,7 +11,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-12T17:22:21.780Z",
+    "time": "2021-10-12T17:30:13.102Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
