8ae2e8a09997f4713a4220aee67f6c566f3183ec
MaxR522 <ranjamario@gmail.com>
2021-10-12 14:33:09 +0300

    👌 IMPROVE: get all users done

---
+++ src/controllers/user_controller/get_all.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import IUser from '../../interfaces/user_interface';
+import User from '../../models/user';
+import genericError from '../../utils/generic_error';
+
+const GetAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users: Array<IUser> = await User.find({}).select(
+      'firstname lastname email gender',
+    );
+
+    return res.status(200).json({
+      error: false,
+      users,
+    });
+  } catch (error) {
+    genericError(res, error);
+  }
+};
+
+export default GetAllUsers;
--- src/routes/protected/user.routes.ts
+++ src/routes/protected/user.routes.ts
@@ -1,7 +1,10 @@
 import * as Express from 'express';
 import GetOne from '../../controllers/user_controller/get_one';
+import GetAllUsers from '../../controllers/user_controller/get_all';
 
 const router = Express.Router();
+
+router.get('/', GetAllUsers);
 
 router.get('/:id', GetOne);
 
